3f9cfe0e6c39d4d28883f678a9bb9d9950c101b6
pragnagopa <pgopa@microsoft.com>
2017-05-25 12:56:19 -0700

    Fixing merge issues

--- sample/QueueTrigger/index.js
+++ sample/QueueTrigger/index.js
@@ -3,7 +3,6 @@
 
     context.log('DequeueCount=%s', context.bindingData.dequeueCount);
     context.log('InsertionTime=%s', context.bindingData.insertionTime);
-    context.log('workItem Text=%s', workItem.text);
-    context.bindings.workItemOut = "new message";
+
     context.done(null, workItem);
 }
